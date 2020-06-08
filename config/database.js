b252359aced2deb6e9f79220c1df8eca0472d243
Marcos Marrero <marcos@Marcos.local>
2020-06-08 21:14:26 +0100

    fix port

--- config/database.js
+++ config/database.js
@@ -6,8 +6,8 @@ module.exports = ({ env }) => ({
       settings: {
         host: env('DATABASE_HOST', 'cluster0-kr4n2.mongodb.net'),
         srv: env.bool('DATABASE_SRV', true),
-        port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'signaturePD'),
+        port: env.int('DATABASE_PORT', 80),
+        database: env('DATABASE_NAME', 'signature'),
         username: env('DATABASE_USERNAME', 'Marcos'),
         password: env('DATABASE_PASSWORD', '8uUP6KDEgYCeREos'),
       },
