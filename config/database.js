aecae90f4fca351758eb2b205a20b72162174fbb
Marcos Marrero <marcos@Marcos.local>
2020-06-08 21:45:34 +0100

    update databse config

--- config/database.js
+++ config/database.js
@@ -4,17 +4,11 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        host: env('DATABASE_HOST', 'cluster0-kr4n2.mongodb.net'),
-        srv: env.bool('DATABASE_SRV', true),
-        port: env.int('DATABASE_PORT', 80),
-        database: env('DATABASE_NAME', 'signature'),
-        username: env('DATABASE_USERNAME', 'Marcos'),
-        password: env('DATABASE_PASSWORD', '8uUP6KDEgYCeREos'),
+        uri: env('DATABASE_URI')
       },
       options: {
-        authenticationDatabase: env('AUTHENTICATION_DATABASE', 'admin'),
-        ssl: env.bool('DATABASE_SSL', true)
-      },
-    },
-  },
-});
+        ssl: true
+      }
+    }
+  }
+})
