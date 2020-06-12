90a55789404e375c97e7a5ad33abf5f63365295d
Marcos Marrero <marcos@Marcos.local>
2020-06-12 14:20:26 +0100

    server changes

--- config/database.js
+++ config/database.js
@@ -4,16 +4,10 @@ module.exports = ({ env }) => ({
     default: {
       connector: "mongoose",
       settings: {
-        host: env("DATABASE_HOST", "127.0.0.1"),
-        srv: env.bool("DATABASE_SRV", false),
-        port: env.int("DATABASE_PORT", 27017),
-        database: env("DATABASE_NAME", "signaturePD"),
-        username: env("DATABASE_USERNAME", ""),
-        password: env("DATABASE_PASSWORD", ""),
-        password: env("DATABASE_PASSWORD", ""),
         uri: env("DATABASE_URI"),
       },
       options: {
+        authenticationDatabase: env("AUTHENTICATION_DATABASE", "admin"),
         ssl: env.bool("DATABASE_SSL", false),
       },
     },
